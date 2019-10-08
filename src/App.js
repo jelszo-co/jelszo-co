2127b49f152903339f8872433069bff1ec0679ae
Tasztalos Pista <tasztalos69@gmail.com>
2019-10-08 18:00:26 +0200

    Add center rect + install packages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
+import "gsap/all";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+export default class App extends Component {
+	render() {
+		return (
+			<div className='App'>
+				<div id='rect-main'></div>
+				<div className='rect-extension rect-extension-1'></div>
+				<div className='rect-extension rect-extension-2'></div>
+				<div id='landing-center-wrapper'>
+					<h1 id='landing-center-text'>Innovativity</h1>
+					<span id='playhead'></span>
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
